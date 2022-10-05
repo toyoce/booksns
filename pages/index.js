52d618bd24ef17507e13437948d3eeb1a754ca22
toyoce <toyo.naoki0701@gmail.com>
2022-10-05 19:07:03 +0900

    トップ画面にgetServerSidePropsを追加

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,16 @@ import Container from '@mui/material/Container';
 import InputAdornment from '@mui/material/InputAdornment';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import axios from 'axios';
 import { useState } from 'react';
 import Link from '../src/Link';
 
-const Home = () => {
+const Home = ({ highlyRatedBooks }) => {
   const [title, setTitle] = useState('');
 
   const handleTextChange = (event) => {
     setTitle(event.target.value);
   };
-
-  const bookUrls = [];
 
   return (
     <Container>
@@ -46,9 +45,9 @@ const Home = () => {
         </Link>
       </Box>
       <Box sx={{ mt: 1, display: "flex", flexWrap: "wrap" }}>
-        {bookUrls.map((url) => (
-          <Box sx={{ mr: 3, mb: 1, border: 1, borderColor: "grey.400" }}>
-            <img src={url} width="136" />
+        {highlyRatedBooks.map((book) => (
+          <Box key={book.isbn} sx={{ mr: 3, mb: 1, border: 1, borderColor: "grey.400" }}>
+            <img src={book.img} width="136" />
           </Box>
         ))}
       </Box>
@@ -57,3 +56,10 @@ const Home = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/highly-rated-books`);
+  const highlyRatedBooks = res.data.bookrecords;
+
+  return { props: { highlyRatedBooks } };
+};
