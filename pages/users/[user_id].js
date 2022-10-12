7a5ac129a3543d8061f1caf87ec6725b5da8e431
toyoce <toyo.naoki0701@gmail.com>
2022-10-12 18:51:23 +0900

    ユーザーページを修正

--- pages/users/[user_id].js
+++ pages/users/[user_id].js
@@ -1,14 +1,27 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
 
-const UserPage = ({ bookrecords }) => {
+const UserPage = ({ user }) => {
   const bookrecordRows = (
     <Box>
-      {bookrecords.map((br) => (
-        <Box key={br.isbn} sx={{ py: 2, borderBottom: 1, borderColor: "grey.400" }}>
-          <Typography variant="body2">{br.title}</Typography>
+      {user.bookrecords.map((br) => (
+        <Box key={br.isbn} sx={{ py: 2, display: "flex", borderBottom: 1, borderColor: "grey.400" }}>
+          <Box sx={{ border: 1, borderColor: "grey.400" }}>
+            <img src={br.img} width="80" />
+          </Box>
+          <Box sx={{ ml: 2 }}>
+            <Typography variant="body2">{br.title}</Typography>
+            <Box sx={{ mt: 1, display: "flex" }}>
+              <Rating value={br.star} size="small" readOnly />
+              <Typography variant="body2" sx={{ ml: 1, color: "grey.700" }}>
+                {"(2022/10/11)"}
+              </Typography>
+            </Box>
+            <Typography variant="body2" sx={{ mt: 1 }}>{br.comment}</Typography>
+          </Box>
         </Box>
       ))}
     </Box>
@@ -16,6 +29,9 @@ const UserPage = ({ bookrecords }) => {
   
   return (
     <Container>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        {`${user.user_id} さんのページ`}
+      </Typography>
       {bookrecordRows}
     </Container>
   );
@@ -24,8 +40,11 @@ const UserPage = ({ bookrecords }) => {
 export default UserPage;
 
 export const getServerSideProps = async ({ params }) => {
-  const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/bookrecords/${params.user_id}`);
-  const bookrecords = response.data.bookrecords;
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/users/${params.user_id}`,
+    { params: { withRecords: 1 } }
+  );
+  const user = response.data;
 
-  return { props: { bookrecords } };
+  return { props: { user } };
 };
