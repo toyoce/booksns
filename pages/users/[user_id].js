6355067d8df4f32dd1f323c8ba5241e626ffe3b1
toyoce <toyo.naoki0701@gmail.com>
2022-10-17 19:00:43 +0900

    レビューを登録する導線の一部を実装

--- pages/users/[user_id].js
+++ pages/users/[user_id].js
@@ -1,11 +1,16 @@
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
-import Link from '../../src/Link';
+import { useContext } from 'react';
+import Link, { NextLinkComposed } from '../../src/Link';
+import { UserContext } from '../_app';
 
 const UserPage = ({ user }) => {
+  const { currentUser } = useContext(UserContext);
+
   const bookrecordRows = (
     <Box>
       {user.bookrecords.map((br) => (
@@ -40,6 +45,22 @@ const UserPage = ({ user }) => {
       <Typography variant="h6" sx={{ my: 2 }}>
         {`${user.user_id} さんのページ`}
       </Typography>
+      <Box sx={{ mt: 3, mb: 1, display: "flex", alignItems: "center" }}>
+        <Typography variant="subtitle1" sx={{ flexGrow: 1 }}>
+          レビュー一覧
+        </Typography>
+        {currentUser.userId === user.user_id && (
+          <Button
+            variant="contained"
+            component={NextLinkComposed}
+            to={"/books"}
+            color="info"
+            size="small"
+          >
+            レビューを追加
+          </Button>
+        )}
+      </Box>
       {bookrecordRows}
     </Container>
   );
