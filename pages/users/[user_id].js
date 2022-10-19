1f146507238a6bb97c1bb89c9c362a7dce1e91db
toyoce <toyo.naoki0701@gmail.com>
2022-10-20 00:05:16 +0900

    レビュー削除時の導線を一部実装

--- pages/users/[user_id].js
+++ pages/users/[user_id].js
@@ -3,16 +3,37 @@ import EditIcon from '@mui/icons-material/Edit';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
 import IconButton from '@mui/material/IconButton';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Link, { NextLinkComposed } from '../../src/Link';
 import { UserContext } from '../_app';
 
 const UserPage = ({ user }) => {
   const { currentUser } = useContext(UserContext);
+
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState(undefined);
+
+  const handleDeleteIconClick = (id) => {
+    setSelected(id);
+    setOpen(true);
+  };
+
+  const handleDeleteButtonClick = () => {
+    alert(`bookrecord(${selected}) will be deleted`);
+  };
+
+  const handleDialogClose = () => {
+    setSelected(undefined);
+    setOpen(false);
+  };
 
   const bookrecordRows = (
     <Box>
@@ -35,7 +56,7 @@ const UserPage = ({ user }) => {
                 <IconButton size="small" sx={{ ml: 0.5 }}>
                   <EditIcon fontSize="inherit" />
                 </IconButton>
-                <IconButton size="small">
+                <IconButton size="small" onClick={() => handleDeleteIconClick(br.id)}>
                   <DeleteIcon fontSize="inherit" />
                 </IconButton>
               </Box>
@@ -63,31 +84,51 @@ const UserPage = ({ user }) => {
   );
   
   return (
-    <Container>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        {`${user.user_id} さんのページ`}
-      </Typography>
-      <Box sx={{ mt: 3, mb: 1, display: "flex", alignItems: "center" }}>
-        <Typography variant="subtitle1" sx={{ flexGrow: 1 }}>
-          レビュー一覧
+    <>
+      <Container>
+        <Typography variant="h6" sx={{ my: 2 }}>
+          {`${user.user_id} さんのページ`}
         </Typography>
-        {currentUser.userId === user.user_id && (
-          <Button
-            variant="contained"
-            component={NextLinkComposed}
-            to={{
-              pathname: "/books",
-              query: { addReview: 1 },
-            }}
-            color="info"
-            size="small"
-          >
-            レビューを追加
-          </Button>
-        )}
-      </Box>
-      {bookrecordRows}
-    </Container>
+        <Box sx={{ mt: 3, mb: 1, display: "flex", alignItems: "center" }}>
+          <Typography variant="subtitle1" sx={{ flexGrow: 1 }}>
+            レビュー一覧
+          </Typography>
+          {currentUser.userId === user.user_id && (
+            <Button
+              variant="contained"
+              component={NextLinkComposed}
+              to={{
+                pathname: "/books",
+                query: { addReview: 1 },
+              }}
+              color="info"
+              size="small"
+            >
+              レビューを追加
+            </Button>
+          )}
+        </Box>
+        {bookrecordRows}
+      </Container>
+      <Dialog open={open} onClose={handleDialogClose}>
+        <Box sx={{ p: 1 }}>
+          <DialogContent>
+            <DialogContentText>
+              この本のレビューを削除しますか？
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              color="warning"
+              onClick={handleDeleteButtonClick}
+            >
+              削除
+            </Button>
+          </DialogActions>
+        </Box>
+      </Dialog>
+    </>
   );
 };
 
