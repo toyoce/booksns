208e5784668206a93ab16420a67b770c5906105e
toyoce <toyo.naoki0701@gmail.com>
2022-10-23 15:04:23 +0900

    本のページのUIを改善

--- pages/books/[isbn].js
+++ pages/books/[isbn].js
@@ -1,14 +1,36 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
-import Link from '../../src/Link';
+import { useContext } from 'react';
+import Link, { NextLinkComposed } from '../../src/Link';
+import { UserContext } from '../_app';
 
 const BookPage = ({ book }) => {
+  const { currentUser } = useContext(UserContext);
+  
+  let myReview, otherUsersReviews;
+  
+  if (currentUser.userId) {
+    myReview = book.bookreviews.filter((br) => (
+      br.user_id === currentUser.userId
+    ))[0];
+    otherUsersReviews = book.bookreviews.filter((br) => (
+      br.user_id !== currentUser.userId
+    ));
+  } else {
+    myReview = undefined;
+    otherUsersReviews = book.bookreviews;
+  }
+
   const bookreviewRows = (
     <Box>
-      {book.bookreviews.map((br) => (
+      {otherUsersReviews.map((br) => (
         <Box key={br.user_id} sx={{ py: 2, borderBottom: 1, borderColor: "grey.400" }}>
           <Box sx={{ display: "flex" }}>
             <Link
@@ -48,8 +70,55 @@ const BookPage = ({ book }) => {
           <Typography variant="body2" sx={{ mt: 1 }}>{book.description}</Typography>
         </Box>
       </Box>
-      <Typography variant="subtitle1" sx={{ mt: 4 }}>レビュー一覧</Typography>
-      {book.bookreviews.length ? bookreviewRows: (
+      {myReview && (
+        <>
+          <Typography variant="subtitle1" sx={{ mt: 4 }}>自分のレビュー</Typography>
+          <Box sx={{ mt: 2, display: "flex", alignItems: "center" }}>
+            <Rating value={myReview.star} size="small" readOnly />
+            <Typography
+              variant="body2"
+              sx={{ ml: 1, color: "grey.700" }}
+            >
+              {"(2022/10/11)"}
+            </Typography>
+            <IconButton
+              component={NextLinkComposed}
+              to={`/bookreviews/${myReview.id}/edit`}
+              size="small"
+              sx={{ ml: 0.5 }}
+            >
+              <EditIcon fontSize="inherit" />
+            </IconButton>
+            <IconButton size="small" onClick={() => alert("delete?")}>
+              <DeleteIcon fontSize="inherit" />
+            </IconButton>
+          </Box>
+          <Typography variant="body2" sx={{ mt: 1 }}>{myReview.comment}</Typography>
+        </>
+      )}
+      <Box sx={{ mt: 4, display: "flex", alignItems: "center" }}>
+        <Typography
+          variant="subtitle1"
+          sx={{ flexGrow: 1 }}
+        >
+          レビュー一覧
+        </Typography>
+        {(currentUser.userId && !myReview) && (
+          <Button
+            variant="contained"
+            component={NextLinkComposed}
+            to={{
+              pathname: "/bookreviews/new",
+              query: { isbn: book.isbn },
+            }}
+            color="info"
+            size="small"
+          >
+            レビューを作成
+          </Button>
+        )}
+      </Box>
+      {otherUsersReviews.length ? bookreviewRows: (
         <Typography variant="body2" sx={{ mt: 6, textAlign: "center" }}>
           まだレビューはありません
         </Typography>
