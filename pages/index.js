bc4a4fd1c34d232c48ba7fc34fd24292860365bf
toyoce <toyo.naoki0701@gmail.com>
2022-11-14 19:50:46 +0900

    ビルドエラーへの対処

--- pages/index.js
+++ pages/index.js
@@ -91,6 +91,7 @@ const Home = ({ highlyRatedBooks, mostReviewedBooks }) => {
       </Container>
       <AppBar color="transparent" position="static" sx={{ mt: 8 }}>
         <Toolbar>
+          { /* eslint-disable-next-line react/jsx-no-target-blank */ }
           <a href="https://webservice.rakuten.co.jp/" target="_blank"><img src="https://webservice.rakuten.co.jp/img/credit/200709/credit_31130.gif" border="0" alt="Rakuten Web Service Center" title="Rakuten Web Service Center" width="311" height="30"/></a>
         </Toolbar>
       </AppBar>
